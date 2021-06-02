44700919937bbb21cee2808bdba55d462f687e6c
Yash Raj Mani <yashrajmani15946@gmail.com>
2021-06-02 12:26:47 +0530

    commit

---
+++ new.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+    cout<<"this is 2nd";
+   
+return 0;
+}
