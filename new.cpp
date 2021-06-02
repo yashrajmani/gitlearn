a02588f2fa98902531c03f03918d0816dbebe3f9
Yash Raj Mani <yashrajmani15946@gmail.com>
2021-06-02 12:29:50 +0530

    part 2

--- new.cpp
+++ new.cpp
@@ -3,12 +3,7 @@ using namespace std;
 int main(){
     cout<<"this is 2nd";
     cout<<"this is 2nd";
-    cout<<"this is 2nd";
-    cout<<"this is 2nd";
-    cout<<"this is 2nd";
-    cout<<"this is 2nd";
-    cout<<"this is 2nd";
-    cout<<"this is 2nd";
+
    
 return 0;
 }
